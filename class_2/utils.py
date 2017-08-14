70bbdaef3a938d5ba4eb5f9095c63ada57806b53
Marc <mr.moreaux@gmail.com>
2017-08-14 09:01:22 +0200

    work on convolutions

--- class_2/utils.py
+++ class_2/utils.py
@@ -1,5 +1,62 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
+import keras
+from keras.datasets import mnist
+
+
+# Load MNIST
+def load_MNIST():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    # Normalize the data
+    x_train = x_train/255.
+    x_test = x_test/255.
+
+    # To categorical
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+# Load a dataset of sighnal going upwards and downwards
+def load_up_down(n_samples=60):
+
+    def signal(a, b):
+        # Returs f(x) = ax + b + epsilon
+        x = np.array(range(5))
+        y = a * x + b + np.random.rand(5) - .5
+        return y
+
+    # Generate up signals
+    a = np.random.rand(n_samples) + 1
+    b = np.random.rand(n_samples) - .5
+    up = map(signal, a, b)
+
+    # Generate down signals
+    a = np.random.rand(n_samples) - 2
+    b = np.random.rand(n_samples) - .5
+    down = map(signal, a, b)
+
+    # Build a dataset of them
+    X = up + down
+    Y = [0 for _ in range(n_samples)]
+    Y += [1 for _ in range(n_samples)]
+
+    # Randomize
+    rand_idx = list(range(n_samples * 2))
+    np.random.shuffle(rand_idx)
+    X = np.array([X[i] for i in rand_idx])
+    Y = np.array([Y[i] for i in rand_idx])
+
+    # Assign train and test
+    n_train_samples = n_samples * 2 * 80 / 100
+    x_train, y_train = X[:n_train_samples], Y[:n_train_samples]
+    x_test, y_test = X[n_train_samples:], Y[n_train_samples:]
+
+    return (x_train, y_train), (x_test, y_test)
+
 
 # Basic functions
 exp = lambda x: np.exp(x + 1e-6)  # any tensor
@@ -69,9 +126,9 @@ class Linear(object):
         self.arg = name
         self.units = units
         self.name = name
-        self.activation = activation or lambda x: x
+        self.activation = activation or (lambda x: x)
         self.activation = np.vectorize(self.activation)
-        self.kernel_init = kernel_init or lambda x: 0
+        self.kernel_init = kernel_init or (lambda x: 0)
         self.kernel_init = np.vectorize(self.kernel_init)
         self.in_size = in_size
         
@@ -86,3 +143,102 @@ class Linear(object):
 
     def backward(self, X):
         return X
+
+
+class Trainer(object):
+    """This class trains a simple linear regression
+
+    params :
+      X, Y : train set (X) and objective (Y)
+      Ws : list with the Weights of the model
+      alphas : learning rate of every weights
+
+    Quickly, you also have to define some function :
+    Pred: the prediction function p(X, Ws)
+    Loss: The loss function L(Y, P)
+    dWs: list of function to compute the gradients
+    """
+    def __init__(self, X, Y, Ws, alphas):
+        super(Trainer, self).__init__()
+        self.X = X
+        self.Y = Y
+        self.Ws = list(Ws)
+        self.alphas = (alphas) * len(Ws) if type(alphas) == 'int' else alphas
+        self.dWs = None
+        self.pred = None
+        self.loss = None
+        self.losses = []
+
+    def train(self, n_steps=100):
+        for i, m_zip in enumerate(zip(self.Ws, self.dWs, self.alphas)):
+            w, dw, alpha = m_zip
+            self.Ws[i] = w - alpha * dw()
+
+    def animated_train(self, is_notebook=False):
+        # Draw initial plot
+        # fig = plt.figure()
+        # ax = plt.axes(xlim=(0, 23), ylim=(0, 1500))
+        fig, axs = plt.subplots(3, 1)
+
+        # Subplot1
+        axs[0].set_xlim(0, 23)
+        axs[0].set_ylim(0, 1500)
+        # axs[0].scatter(self.X[0], self.Y, s=1)
+        line, = axs[0].plot([], [], lw=1, c='r')
+
+        # Subplot2 - 
+        X_ = range(len(self.X[0]))
+        axs[1].set_ylim(self.Y.min() * 0.9, self.Y.max() * 1.1)
+        axs[1].plot(X_, self.Y, lw=1)
+        line2, = axs[1].plot([], [], lw=1, c='r')
+
+        # Subplot3 - loss
+        X_ = range(500)
+        axs[2].set_xlim(0, 100)
+        axs[2].set_ylim(0, 1e0)
+        line3, = axs[2].plot([], [], lw=1, c='r')
+
+        # Initialization function: plot the background of each frame
+        def init():
+            line.set_data([], [])
+            line2.set_data([], [])
+            line3.set_data([], [])
+            return line, line2, line3,
+
+        # Animation function.
+        def animate(i, *fargs):
+            self = fargs[0]
+            if i == 0:
+                del self.losses[:]
+
+            # Train
+            self.train(50)
+
+            # Subplot1
+            # X_ = [range(0, 25), ]
+            # Y_ = self.pred(X_)
+            # line.set_data(X_[0], Y_)
+
+            # # Subplot2 - Y render
+            # X_ = [range(len(self.X[0])), ]
+            # Y_ = self.pred(self.X)
+            # line2.set_data(X_[0], Y_)
+
+            # Subplot3 - loss
+            self.losses.append(self.loss())
+            X_ = range(i + 1)
+            Y_ = self.losses
+            line3.set_data(X_, Y_)
+
+            return line, line2, line3,
+
+        # Call the animator.
+        anim = animation.FuncAnimation(fig, animate, frames=100,
+                                       init_func=init, fargs=[self, ],
+                                       interval=20, blit=True)
+        if is_notebook is True:
+            plt.close(anim._fig)
+            return anim
+        else:
+            plt.show()
+
